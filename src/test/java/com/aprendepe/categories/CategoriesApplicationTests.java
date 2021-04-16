74e9f5351acc0e6a531bf564eed811ad71c88253
Gorki Rubiños Soto <grubinos@outlook.com>
2021-04-16 11:06:59 -0500

    First commit

---
+++ src/test/java/com/aprendepe/categories/CategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendepe.categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
